a76fbe0f4761f38c265ca593b551fa749cee08a8
Milla22 <marti.ludmilla@orange.fr>
2024-02-19 21:16:05 +0100

    line chart stylisé

--- src/Components/TinyLineChart/index.jsx
+++ src/Components/TinyLineChart/index.jsx
@@ -1,61 +1,50 @@
 import './TinyLineChart.css';
 import React, { PureComponent } from 'react';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { LineChart, Line, XAxis, Tooltip, ResponsiveContainer } from 'recharts';
 
 const data = [
     {
-        name: 'L',
-        uv: 4000,
-        pv: 2400,
-        amt: 2400,
+        day: 'L',
+        sessionLength: 42,
     },
     {
-        name: 'M',
-        uv: 3000,
-        pv: 1398,
-        amt: 2210,
+        day: 'M',
+        sessionLength: 14,
     },
     {
-        name: 'M',
-        uv: 2000,
-        pv: 9800,
-        amt: 2290,
+        day: 'M',
+        sessionLength: 34,
     },
     {
-        name: 'J',
-        uv: 2780,
-        pv: 3908,
-        amt: 2000,
+        day: 'J',
+        sessionLength: 4,
     },
     {
-        name: 'V',
-        uv: 1890,
-        pv: 4800,
-        amt: 2181,
+        day: 'V',
+        sessionLength: 68,
     },
     {
-        name: 'S',
-        uv: 2390,
-        pv: 3800,
-        amt: 2500,
+        day: 'S',
+        sessionLength: 46,
     },
     {
-        name: 'D',
-        uv: 3490,
-        pv: 4300,
-        amt: 2100,
+        day: 'D',
+        sessionLength: 104,
     },
 ];
 
 export default class Example extends PureComponent {
     render() {
         return (
-            <div className ="container-tiny-line-chart">
-                <h3 className="title-tiny-line-chart">Durée moyenne des sessions</h3>
-                <LineChart width={300} height={100} data={data}>
-                    <Line type="monotone" dataKey="pv" stroke="#8884d8" strokeWidth={2} />
-                    <XAxis dataKey="name" />
-                </LineChart>
+            <div className = "container-tiny-line-chart">
+                <h3 className = "title-tiny-line-chart">Durée moyenne des sessions</h3>
+                <ResponsiveContainer width = "100%" height = "100%">
+                    <LineChart margin = {{ bottom: 90, left: 15, right:15 }} data = { data } width = "100%">
+                        <Line type = "monotone" dataKey = "sessionLength" stroke = "#ffffff7f" strokeWidth = { 2.5 } dot = { false } />
+                        <XAxis dataKey = "day" stroke = "#ffffff7f" width = "100%" tickLine = { false } axisLine = { false }/>
+                        <Tooltip cursor = {false} wrapperStyle= {{ outline: "none", fontWeight: 500, color: "black" }}/>
+                    </LineChart>
+                </ResponsiveContainer>
             </div>
         );
     }
