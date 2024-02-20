9e4f1522debd5df653e90ebad1be844920876763
Milla22 <marti.ludmilla@orange.fr>
2024-02-20 17:14:03 +0100

    Mock data sur composant Pie chart

--- src/Components/PieBarChart/index.jsx
+++ src/Components/PieBarChart/index.jsx
@@ -1,6 +1,7 @@
 import './PieBarChart.css';
-import React, { PureComponent } from 'react';
+import React, { useState, useEffect } from 'react';
 import { PieChart, Pie, Cell, ResponsiveContainer } from 'recharts';
+import { USER_MAIN_DATA } from '../../Services/DataMock';
 
 const data = [
     {
@@ -8,25 +9,31 @@ const data = [
         score: 30,
     }
 ];
-const data01 = [
-    { name: 'Group A', value: 2400 },
-]
 
-export default class Example extends PureComponent {
-    render() {
-        return (
-            <div className = "container-pie-bar-chart">
-                <h3 className = "title-pie-chart">Score</h3>
-                <h4 className = "percentage-score">30%</h4><p className = "objectif-score">de votre objectif</p>
-                <ResponsiveContainer height = "100%" width = "100%">
-                    <PieChart width = { 130 } height = { 130 } margin = {{ bottom: 35, top: -35 }} fill="#8884d8">
-                    <Pie data = { data01 } dataKey = "value" cx = "50%" cy = "50%" outerRadius = {70} fill = "white" isAnimationActive = { false } />
-                        <Pie data = { data } dataKey = "score"  startAngle = { 180 } endAngle = { 72 } innerRadius = { "60%"} outerRadius = { "70%" } cornerRadius ={ 10 } >
-                            <Cell fill = "red" cornerRadius = "50%"/>
-                        </Pie>
-                    </PieChart>
-                </ResponsiveContainer>
-            </div>
-        );
-    }
+export default function PieBarChart() {
+    const [ scorePercentage, setScorePercentage ] = useState(0);
+    useEffect(() => {
+        // Fetch data user Cecilia //
+        const fetchUserMainData = async () => {
+            const userData = USER_MAIN_DATA.find( user => user.id === 18 );
+            if ( userData ) {
+                setScorePercentage( userData.score * 100 );
+            }
+        };
+        fetchUserMainData();
+    }, []);
+    return (
+        <div className = "container-pie-bar-chart">
+            <h3 className = "title-pie-chart">Score</h3>
+            <h4 className = "percentage-score">{scorePercentage}%</h4><p className = "objectif-score">de votre objectif</p>
+            <ResponsiveContainer height = "100%" width = "100%">
+                <PieChart width = { 130 } height = { 130 } margin = {{ bottom: 35, top: -35 }} fill="#8884d8">
+                <Pie dataKey = "value" cx = "50%" cy = "50%" outerRadius = { 70 } fill = "white" isAnimationActive = { false } />
+                    <Pie data = { data } dataKey = "score"  startAngle = { 180 } endAngle = { 72 } innerRadius = { "60%"} outerRadius = { "70%" } cornerRadius ={ 10 }>
+                        <Cell fill = "red" cornerRadius = "50%"  />
+                    </Pie>
+                </PieChart>
+            </ResponsiveContainer>
+        </div>
+    );
 }
