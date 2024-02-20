093906006390c165ccf7f78b281536ba7b179ef4
Milla22 <marti.ludmilla@orange.fr>
2024-02-20 16:31:16 +0100

    mock data sur composant radar chart

--- src/Components/SimpleRadarChart/index.jsx
+++ src/Components/SimpleRadarChart/index.jsx
@@ -1,46 +1,33 @@
 import './SimpleRadarChart.css';
-import React, { PureComponent } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Radar, RadarChart, PolarGrid, PolarAngleAxis, ResponsiveContainer } from 'recharts';
+import { USER_PERFORMANCE } from '../../Services/DataMock';
 
-const data = [
-    {
-        kind: 'Intensité',
-        performance: 110,
-    },
-    {
-        kind: 'Vitesse',
-        performance: 220,
-    },
-    {
-        kind: 'Force',
-        performance: 80,
-    },
-    {
-        kind: 'Endurance',
-        performance: 80,
-    },
-    {
-        kind: 'Energie',
-        performance: 240,
-    },
-    {
-        kind: 'Cardio',
-        performance: 200,
-    },
-];
-
-export default class SimpleRadarChart extends PureComponent {
-    render() {
-        return (
-            <div className="container-simple-radar-chart">
-                <ResponsiveContainer width = "100%" height = "100%">
-                    <RadarChart outerRadius ={ 70 }  data = { data }>
-                        <PolarGrid radialLines = { false }/>
-                        <PolarAngleAxis dataKey = "kind" stroke = "white" tickLine = { false } fontSize = { 12 } />
-                        <Radar name = "performance" dataKey = "performance" stroke = "#FF0101" fill = "#FF0101" fillOpacity = { 0.7 } />
-                    </RadarChart>
-                </ResponsiveContainer>
-            </div>    
-        );
-    }
+export default function SimpleRadarChart() {
+    const [ performanceData, setPerformanceData ] = useState([]);
+    useEffect(() => {
+        // Fetch performance data Cecilia //
+        const fetchUserPerformanceData = async () => {
+            const userData = USER_PERFORMANCE.find( user => user.userId === 18 );
+            if (userData) {
+                setPerformanceData(userData.data.map( item => ({
+                    kind: userData.kind[ item.kind ],
+                    performance: item.value
+                })));
+            }
+        };
+        fetchUserPerformanceData();
+    }, []);
+    return (
+        <div className="container-simple-radar-chart">
+            <ResponsiveContainer width = "100%" height = "100%">
+                <RadarChart outerRadius ={ 70 }  data = { performanceData }>
+                    <PolarGrid radialLines = { false }/>
+                    <PolarAngleAxis dataKey = "kind" stroke = "white" tickLine = { false } fontSize = { 12 } />
+                    <Radar name = "performance" dataKey = "performance" stroke = "#FF0101" fill = "#FF0101" fillOpacity = { 0.7 } />
+                </RadarChart>
+            </ResponsiveContainer>
+        </div>    
+    );
 }
+
