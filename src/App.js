e34108c3205c2a7fa41d7e5751a4d898e407a1d7
Milla22 <marti.ludmilla@orange.fr>
2024-02-21 17:00:07 +0100

    implémentation du composant page Home pour dans l'app.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,14 @@
 import './App.css';
 import Header from '../src/Components/Header/index.jsx';
 import VerticalLayout from '../src/Components/VerticalLayout/index.jsx';
-import Introduction from './Components/Introduction/index.jsx';
-import SimpleBarChart from './Components/SimpleBarChart/index.jsx';
-import SimpleRadarChart from './Components/SimpleRadarChart/index.jsx';
-import TinyLineChart from './Components/TinyLineChart/index.jsx';
-import PieBarChart from './Components/PieBarChart/index.jsx';
-import AsideColumn from '../src/Components/AsideColumn/index.jsx';
-
+import Home from '../src/Components/pages/Home/index.jsx';
 
 function Dashboard() {
     return (
         <div className = "container-dashboard">
             <Header />
             <VerticalLayout />
-            <div className = "dash-center">
-                <Introduction />
-                <SimpleBarChart />
-                <AsideColumn />
-                <div className ='charts-row'>
-                    <TinyLineChart />
-                    <SimpleRadarChart />
-                    <PieBarChart />
-                </div>
-            </div>
+            <Home />
         </div>
     );
 }
---
+++ src/Components/pages/Home/index.jsx
@@ -0,0 +1,26 @@
+import './Home.css';
+import Introduction from '../../Introduction/index.jsx';
+import SimpleBarChart from '../../SimpleBarChart/index.jsx';
+import SimpleRadarChart from '../../SimpleRadarChart/index.jsx';
+import TinyLineChart from '../../TinyLineChart/index.jsx';
+import PieBarChart from '../../PieBarChart/index.jsx';
+import AsideColumn from '../../AsideColumn/index.jsx';
+
+
+function Home() {
+    return (
+        <div className = "container-home">
+            <div className = "dash-center">
+                <Introduction />
+                <SimpleBarChart />
+                <AsideColumn />
+                <div className ='charts-row'>
+                    <TinyLineChart />
+                    <SimpleRadarChart />
+                    <PieBarChart />
+                </div>
+            </div>
+        </div>
+    );
+}
+export default Home;
