6cba742d0dcafc7a03c5feb45de515a85a6a9042
Milla22 <marti.ludmilla@orange.fr>
2024-02-27 16:44:46 +0100

    Chargement des datas composant PieChart depuis l'Api avec UseApiSportSee

--- src/Components/PieBarChart/index.jsx
+++ src/Components/PieBarChart/index.jsx
@@ -1,4 +1,4 @@
-import './PieBarChart.css';
+/*import './PieBarChart.css';
 import React, { useState, useEffect } from 'react';
 import { PieChart, Pie, Cell, ResponsiveContainer } from 'recharts';
 import { UseApiMockSportSee } from '../../Services/UseApiMockSportSee.js';
@@ -41,6 +41,62 @@ export default function PieBarChart() {
             </ResponsiveContainer>
         </div>
     );
+}*/
+
+import './PieBarChart.css';
+import React, { useState, useEffect } from 'react';
+import { PieChart, Pie, Cell, ResponsiveContainer } from 'recharts';
+import { getUserMainData } from '../../Services/UseApiSportSee.js';
+
+export default function PieBarChart() {
+    const [scorePercentage, setScorePercentage] = useState(0);
+
+    useEffect(() => {
+        const fetchUserData = async () => {
+            try {
+                // Récupération de l'ID utilisateur depuis les paramètres de l'URL //
+                const params = new URLSearchParams(window.location.search);
+                const userId = parseInt(params.get('user') ?? 12);
+
+                // Récupération des données principales de l'utilisateur en fonction de l'ID //
+                const userData = await getUserMainData(userId);
+                const scoreKey = userId === 12 ? 'todayScore' : 'score'; // Choix de la clé en fonction de l'ID utilisateur //
+
+                // Obtention du score pourcentage en fonction de la clé choisie //
+                const score = userData.data[scoreKey] ?? 0;
+                setScorePercentage(score * 100);
+            } catch (error) {
+                console.error('Erreur lors de la récupération des données utilisateur :', error);
+            }
+        };
+        fetchUserData();
+    }, []);
+
+    const startAngle = 180;
+    const endAngle = startAngle - (360 * scorePercentage / 100); // Calcul de l'angle de fin en soustrayant le pourcentage du score à l'angle de début //
+
+    const data = [
+        {
+            name: 'Score',
+            score: scorePercentage,
+        }
+    ];
+
+    return (
+        <div className="container-pie-bar-chart">
+            <h3 className="title-pie-chart">Score</h3>
+            <h4 className="percentage-score">{scorePercentage}%</h4><p className="objectif-score">de votre objectif</p>
+            <ResponsiveContainer height="100%" width="100%">
+                <PieChart width={130} height={130} margin={{ bottom: 35, top: -35 }}  >
+                    <Pie data={data} dataKey="score" startAngle={startAngle} endAngle={endAngle} innerRadius={"60%"} outerRadius={"70%"} cornerRadius={10} >
+                        <Cell fill="red" cornerRadius="50%" />
+                    </Pie>
+                </PieChart>
+            </ResponsiveContainer>
+        </div>
+    );
 }
+
+
 
 
