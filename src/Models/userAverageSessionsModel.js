169ba4098ebea495adb884dd51d66844fcf49988
Milla22 <marti.ludmilla@orange.fr>
2024-02-29 10:50:17 +0100

    Impléméntation model userAverageSessions prop-types

--- src/Models/userAverageSessionsModel.js
+++ src/Models/userAverageSessionsModel.js
@@ -1,10 +1,12 @@
 // Modèle pour les durees moyennes sessions d'activité de l'utilisateur //
-const UserAverageSessionsModel = {
-    sessions: [
-        {
-            day: Number,
-            sessionLength: Number
-        }
-    ]
-}
-export { UserAverageSessionsModel };
+import PropTypes from 'prop-types';
+
+const UserAverageSessionsModel = PropTypes.shape({
+    userId: PropTypes.number.isRequired,
+    sessions: PropTypes.arrayOf(PropTypes.shape({
+        day: PropTypes.number.isRequired,
+        sessionLength: PropTypes.number.isRequired
+    })).isRequired
+});
+
+export default UserAverageSessionsModel;
