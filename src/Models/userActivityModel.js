1a1c1cc04b3cc66cf77cb31b1e2edeb8b6a95f89
Milla22 <marti.ludmilla@orange.fr>
2024-02-29 10:47:37 +0100

    Implémentation model UserActivity prop-types

--- src/Models/userActivityModel.js
+++ src/Models/userActivityModel.js
@@ -1,11 +1,13 @@
 // Modèle pour les sessions d'activité de l'utilisateur //
-const UserActivityModel = {
-    sessions: [
-        {
-            day: String,
-            kilogram: Number,
-            calories: Number
-        }
-    ]
-}
-export { UserActivityModel };
+import PropTypes from 'prop-types';
+
+const UserActivityModel = PropTypes.shape({
+    userId: PropTypes.number.isRequired,
+    sessions: PropTypes.arrayOf(PropTypes.shape({
+        day: PropTypes.string.isRequired,
+        kilogram: PropTypes.number.isRequired,
+        calories: PropTypes.number.isRequired
+    })).isRequired
+});
+
+export default UserActivityModel;
