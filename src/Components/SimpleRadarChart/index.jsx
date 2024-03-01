aa773e5475bfed8d8aa6e94c0db8e853f7f027e1
Milla22 <marti.ludmilla@orange.fr>
2024-03-01 13:44:28 +0100

    Validation data RadarChart model propTypes

--- src/Components/SimpleRadarChart/index.jsx
+++ src/Components/SimpleRadarChart/index.jsx
@@ -2,6 +2,8 @@ import './SimpleRadarChart.css';
 import React, { useState, useEffect } from 'react';
 import { Radar, RadarChart, PolarGrid, PolarAngleAxis, ResponsiveContainer } from 'recharts';
 import { getUserPerformance } from '../../Services/UseApiSportSee'; 
+import PropTypes from 'prop-types';
+import { UserPerformanceModel } from '../../Models/userPerformanceModel.js';
 
 export default function SimpleRadarChart() {
     const [performanceData, setPerformanceData] = useState([]);
@@ -13,6 +15,7 @@ export default function SimpleRadarChart() {
                 let userId = params.get('user') ?? 12;
                 userId = parseInt(userId);
                 const userData = await getUserPerformance(userId);
+                checkUserPerformanceData(userData); // Appel de la fonction de validation //
                 const kindData = userData.data.kind; // Récupére les noms des kinds à partir des data API //
                 const performanceData = userData.data.data.map(item => ({
                     kind: kindData[item.kind], // Utilise l'objet kind pour obtenir le nom du kind //
@@ -25,6 +28,21 @@ export default function SimpleRadarChart() {
         };
         fetchUserPerformanceData();
     }, []);
+
+    // Fonction de validation des données de performance utilisateur //
+    const checkUserPerformanceData = (data) => {
+        // Vérifie si les données ne sont pas définies ou si des propriétés essentielles sont absentes //
+        if (!data.data || !data.data.userId || !data.data.kind || !data.data.data || !Array.isArray(data.data.data)) {
+            console.error("Données de performance utilisateur manquantes ou incorrectes :", data);
+            return; // Arrête l'exécution de la fonction si les données sont incorrectes //
+        }     
+        // Vérifie si chaque élément de données correspond au modèle UserPerformanceModel //
+        if (data.data.userId && data.data.kind && data.data.data && Array.isArray(data.data.data)) {
+            PropTypes.checkPropTypes(UserPerformanceModel, data, 'data', 'SimpleRadarChart');
+        }       
+        console.log("Modèle de données de performance utilisateur :", UserPerformanceModel); 
+        console.log("Données de performance utilisateur validées :", data); 
+    };
 
     return (
         <div className="container-simple-radar-chart">
--- src/Models/userPerformanceModel.js
+++ src/Models/userPerformanceModel.js
@@ -1,13 +1,13 @@
 // Modèle pour les performances de l'utilisateur //
 import PropTypes from 'prop-types';
 
-const UserPerformanceModel = PropTypes.shape({
+export const UserPerformanceModel = PropTypes.shape({
     userId: PropTypes.number.isRequired,
     kind: PropTypes.objectOf(PropTypes.string.isRequired).isRequired,
     data: PropTypes.arrayOf(PropTypes.shape({
         value: PropTypes.number.isRequired,
         kind: PropTypes.number.isRequired
     })).isRequired
-});
+}).isRequired;
 
 export default UserPerformanceModel;
